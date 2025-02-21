c27379a2718e3de867bc19e17ba72a58d84a63ed
Ranjith-varma <vikasvarma1303@gmail.com>
2025-02-21 22:23:58 +0530

    commit

--- 20-Feb/task.py
+++ 20-Feb/task.py
@@ -52,7 +52,7 @@ else:
 side1=int(input("Enter Side1:"))
 side2=int(input("Enter Side2:"))
 side3=int(input("Enter Side3:"))
-if side1>0 or side2>0 or side3>0:
+if side1<=0 or side2<=0 or side3<=0:
     print("Enter valid Number Sides must be greater than 0")
 elif side1+side2>side3 and side2+side3>side1 and side1+side3>side2:
     print("It is a valid Triangle")
